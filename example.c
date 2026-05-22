995ce42cd94f50873ac14039a63b945a845401ce
x3hy <tkf.x1os@gmail.com>
2026-05-22 14:39:22 +1200

    removed junk, changed formatting

--- example.c
+++ example.c
@@ -1,20 +1,20 @@
 #include "arglib.h"
 #include <stdio.h>
 
-int main(int argc, char *argv[]){
+int main (int argc, char *argv[]) {
+
 	// For each argument
 	FORARGS {
-
 		// Can run single line code after statement
-		ARG("--help") printf("Help called!\n");
+		ARG ("--help") printf ("Help called!\n");
 
-		// get the value of the argument
-		ARG("--test") printf("test run with value: %s\n", ARGVAL);
+		// Get the value of the argument
+		ARG ("--test") printf ("test run with value: %s\n", ARGVAL);
 
 		// Multi-line code execution
-		ARG("--alt-test"){
-			printf("test123\n");
-			printf("test321\n");
+		ARG ("--alt-test") {
+			printf ("test123\n");
+			printf ("test321\n");
 		}
 	}
 
