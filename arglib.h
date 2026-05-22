06f82857a5e89a57224dad02a882c779735cdb6b
x3hy <tkf.x1os@gmail.com>
2026-05-22 13:52:52 +1200

    finished library

--- arglib.h
+++ arglib.h
@@ -1,72 +1,101 @@
 #ifndef ARGLIB_H
 #define ARGLIB_H
 
-// Dependencies
-#include <stdint.h>
-#include <stdarg.h>
-#include <limits.h>
+#define ARGLIB_MAX_ARGLEN 256
+static int  __arg_exitcode  =  0;
+static char __arg_splitchar = '=';
 
-// Type used for properties, increase this to a
-// uint16_t if you have custom properties
-#define _ARG_PROP_TYPE uint8_t
 
-// Possible opts, refer to the _ARG_PROP_TYPE
-// if you intend on adding more properties
-enum {
-	UNIUQE,
-	NO_VALUE,
-	TAKES_VALUE,
-	NOT_NULL,
-	FIRST_ARG,
-	LAST_ARG,
-	SINGLE_USE
-};
+/* Function declarations */
+static int _is_arg_match(char *pattern, char *arg);
 
-// prior is uint8_t this value will be 8
-#define _ARG_MAX_PROP (int)(sizeof(_PROP_TYPE))
+/* macroslop */
 
-// Gets the count of VA_ARGS
-#define _VA_ARGS_COUNT(...) \
-	sizeof((int[]){0, __VA_ARGS__}) / sizeof(int)
-#define OPT(...) \
-	(_VA_ARGS_COUNT(__VA_ARGS__) ? 0 : arglib_combine_opts())
+/* Loops through arguments given, av and ac are equ to
+ * argv and argc. */
+#define arg_each(_argv, _argc) \
+	for (int __arg_i = 1; __arg_i < _argc; __arg_i++)
 
-/**
- * Combines given values into a singular prop type
- * See: OPT
- **/
-static _ARG_PROP_TYPE
-arg_combine_opts(int count, ...)
-{
-	if (count > _ARG_MAX_PROP || count <= 0)
-		return 0u;
+#define avalue argv[__arg_i]
 
-	// Fetch va arg list
-	va_list lst;
-	va_start(lst, count);
-	_ARG_PROP_TYPE out = 0u;
+/* Uses a switch case to allow for inline code execution.
+ * the `switch` line also assigns __arg_success the return
+ * code of _is_arg_match which checks if a given argument
+ * matches the given pattern. */
+#define acase(arg) \
+	switch(__arg_exitcode = _is_arg_match(arg, avalue)) \
+		case 0:
 
-	// for each given opt
-	for (int i = 0; i < count; i++)
-		out = out ^ (1u << va_arg(lst, int));
-
-	va_end(lst);
-	return out;
+static int _arglib_strlen(char *str){
+	if (!str) return 1;
+	int str_s = 0;
+	while (str[str_s])
+		str_s++;
+	return str_s+1;
 }
 
-// linked list for the values in the argument list
-typedef struct arg_value {
-	char *value;
-	int index;
-	struct arg_value *next;
-} arg_value;
+static int _arglib_strcmp(char *s1, char *s2){
+	if (!s1 || !s2) return -1;
+	while (*s1 && !(*s1 - *s2)){
+		s1++;
+		s2++;
+	}
+	
+	return *s1 - *s2;
+}
 
-// linked list for the arguments
-typedef struct arg_linked_list {
-	char *flag;
-	char *desc;
-	struct arg_linked_list *next;
-} arg_linked_list;
+#include <stdio.h>
+static int _is_arg_match(char *pattern, char *arg){
+	const int arg_len = _arglib_strlen(arg);
+	if (arg_len > arg_len)
+		// Argument exceeds limit
+		return -1;
 
+	// Locate the splitchar within the argument
+	int split = 0;
+	while (arg[split]){
+		if (arg[split] == __arg_splitchar)
+			break;
+		split++;
+	}
+
+	// Save a copy of argument for later :3
+	char undobuf[arg_len];
+	for (int i = 0; i < arg_len; i++)
+		undobuf[i] = arg[i];
+
+	// Separate the key and value from the argument
+	// taken from x3hy/plib/plib6.h
+	//
+	// TODO: this can be optimised
+	char key[arg_len - split];
+	for (int i = 0; i < arg_len; i++){
+		if (i <= (split != arg_len-1 ? split : arg_len)){
+			key[i] = arg[i];
+			if (split && i == split)
+				key[i] = '\0';
+			else if (i == arg_len)
+				arg[0] = '\0';
+		}
+
+		// Copy all the content after the split
+		// back to arg. This will leave arg with
+		// just the last half (after the splitchar).
+		if (split != arg_len -1)
+			arg[i] = arg[i + split + 1];
+	}
+
+	// now we have the following:
+	// key: first half of the argument (before the splitchar)
+	// arg: last half of the argument (after the splitchar)
+	if (_arglib_strcmp(pattern, key) == 0)
+		return 0;
+
+	// reset the argument using undobuf
+	for (int i = 0; i < arg_len; i++)
+		arg[i] = undobuf[i];
+
+	return 1;
+}
 
 #endif//ARGLIB_H
--- example.c
+++ example.c
@@ -2,14 +2,24 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]){
-	struct arguments[] = {
-		CAT("basic", OPT(ENABLED),
-			{"--help", "Shows the help menu", OPT(TAKES_VALUE, NOT_NULL)},
-			{"--test",
-		),
-		CAT("other", OPT(ENABLED),
-			{"--test2", "test argument", OPT(TAKES_VALUE, UNIQUE)},
-			{"--test3", "another test", OPT(NO_VALUE, LAST_ITEM)}
-		)
-	};
+	// for each argument
+	arg_each(argv, argc){
+
+		// can run single line code after statement
+		acase("--help") printf("Help called!\n");
+
+		// get the value of the argument
+		acase("--test") printf("test run with value: %s\n", avalue);
+
+		// multi-line code execution
+		acase("--alt-test"){
+			printf("test123\n");
+			printf("test321\n");
+		}
+
+		// print some extra info
+		printf("value: %s, exitcode: %d\n", avalue, __arg_exitcode);
+	}
+
+	return 0;
 }
